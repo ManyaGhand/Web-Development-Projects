0462537396b9abd5197342e3a21f87b4809a9ded
ManyaGhand <manyaghand@gmail.com>
2025-08-22 21:28:11 +0530

    Add files via upload
    100 Best Movies Web Scraper
    A Python project that scrapes Empire Online’s “100 Greatest Movies of All Time” list and saves the results into a structured file.
    
    Features:
    	•	Scrapes movie titles from Empire Online’s Top 100 Movies page
    	•	Stores results in a clean, ordered list
    	•	Exports data into a text file for easy use
    	•	Built with BeautifulSoup4 and Requests
    
    Tech Stack:
    	•	Python
    	•	BeautifulSoup (for parsing HTML)
    	•	Requests (for fetching webpage data)
    
    This project is a simple yet powerful example of using web scraping to collect and organize information from real websites.

---
+++ MoviesList/main.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://web.archive.org/web/20200518073855/"
+                        "https://www.empireonline.com/movies/features/best-movies-2/")
+
+web_page = response.text
+soup = BeautifulSoup(web_page, "html.parser")
+
+movies_list = [movie.getText() for movie in soup.find_all(name="h3", class_="title")]
+movies_list.reverse()
+
+with open("movies.txt", "w", encoding= "utf-8") as file:
+    for movie in movies_list:
+        file.write(movie + "\n")
